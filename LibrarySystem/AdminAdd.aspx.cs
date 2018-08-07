140ddb02e0afa2219ea453cbad0d36e7856bd5a1
ZackE4 <42150739+ZackE4@users.noreply.github.com>
2018-08-07 18:08:24 -0400

    profile stuff

--- LibrarySystem/AdminAdd.aspx.cs
+++ LibrarySystem/AdminAdd.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,5 +12,383 @@ public partial class AdminAdd : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void ddSelector_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = ddSelector.SelectedIndex;
+    }
+
+    protected void btnAddGenre_Click(object sender, EventArgs e)
+    {
+        bool addSuccess = true;
+        SqlConnection LibConnect = new SqlConnection();
+        LibConnect.ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["NetClassConnectionString"].ConnectionString;
+        SqlCommand cmd = LibConnect.CreateCommand();
+
+        string title = tbAddGenreTitle.Text;
+        string rating = tbAddGenreRating.Text;
+
+        if (String.IsNullOrEmpty(title) ||
+            String.IsNullOrEmpty(rating))
+        {
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Please enter data for all fields')", true);
+            addSuccess = false;
+        }
+        else
+        {
+            try
+            {
+                string query = "Insert Into [Genre] (Title, Rating) VALUES(@Title, @Rating)";
+                SqlParameter titleParam = new SqlParameter();
+                titleParam.ParameterName = "@Title";
+                titleParam.Value = title;
+                SqlParameter ratingParam = new SqlParameter();
+                ratingParam.ParameterName = "@Rating";
+                ratingParam.Value = rating;
+
+                cmd.CommandText = query;
+                cmd.Parameters.Add(titleParam);
+                cmd.Parameters.Add(ratingParam);
+
+                LibConnect.Open();
+                var result = cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                addSuccess = false;
+            }
+            finally
+            {
+                cmd.Dispose();
+                LibConnect.Close();
+
+                if (addSuccess)
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "Success", "alert('Genre Added.')", true);
+                    tbAddGenreTitle.Text = "";
+                    tbAddGenreRating.Text = "";
+                }
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Something Went Wrong.')", true);
+                }
+            }
+        }
+    }
+
+    protected void btnAddPub_Click(object sender, EventArgs e)
+    {
+        bool addSuccess = true;
+        SqlConnection LibConnect = new SqlConnection();
+        LibConnect.ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["NetClassConnectionString"].ConnectionString;
+        SqlCommand cmd = LibConnect.CreateCommand();
+
+        string name = tbAddPubName.Text;
+        string addr = tbAddPubAddr.Text;
+        string city = tbAddPubCity.Text;
+        string prov = tbAddPubProv.Text;
+        string country = tbAddPubCountry.Text;
+
+
+
+        if (String.IsNullOrEmpty(name) ||
+            String.IsNullOrEmpty(addr) ||
+            String.IsNullOrEmpty(city) ||
+            String.IsNullOrEmpty(prov) ||
+            String.IsNullOrEmpty(country))
+        {
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Please enter data for all fields')", true);
+            addSuccess = false;
+        }
+        else
+        {
+            try
+            {
+                string query = "Insert Into [Publisher] (Name, Address, City, [Province/State], Country) VALUES(@Name, @Address, @City, @Prov, @Country)";
+                SqlParameter nameParam = new SqlParameter();
+                nameParam.ParameterName = "@Name";
+                nameParam.Value = name;
+                SqlParameter addrParam = new SqlParameter();
+                addrParam.ParameterName = "@Address";
+                addrParam.Value = addr;
+                SqlParameter cityParam = new SqlParameter();
+                cityParam.ParameterName = "@City";
+                cityParam.Value = city;
+                SqlParameter provParam = new SqlParameter();
+                provParam.ParameterName = "@Prov";
+                provParam.Value = prov;
+                SqlParameter countryParam = new SqlParameter();
+                countryParam.ParameterName = "@Country";
+                countryParam.Value = country;
+
+                cmd.CommandText = query;
+                cmd.Parameters.Add(nameParam);
+                cmd.Parameters.Add(addrParam);
+                cmd.Parameters.Add(cityParam);
+                cmd.Parameters.Add(provParam);
+                cmd.Parameters.Add(countryParam);
+
+                LibConnect.Open();
+                var result = cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                addSuccess = false;
+            }
+            finally
+            {
+                cmd.Dispose();
+                LibConnect.Close();
+
+                if (addSuccess)
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "Success", "alert('Publisher Added.')", true);
+                    tbAddPubName.Text = "";
+                    tbAddPubAddr.Text = "";
+                    tbAddPubCity.Text = "";
+                    tbAddPubProv.Text = "";
+                    tbAddPubCountry.Text = "";
+                }
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Something Went Wrong.')", true);
+                }
+            }
+        }
+    }
+
+    protected void btnAddAuthor_Click(object sender, EventArgs e)
+    {
+        bool addSuccess = true;
+        SqlConnection LibConnect = new SqlConnection();
+        LibConnect.ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["NetClassConnectionString"].ConnectionString;
+        SqlCommand cmd = LibConnect.CreateCommand();
+
+        string fname = tbAddAuthorFname.Text;
+        string sname = tbAddAuthorSname.Text;
+        string lname = tbAddAuthorLname.Text;
+        string dob = tbAddAuthorDOB.Text;
+        DateTime dobDate;
+
+        if (String.IsNullOrEmpty(fname) ||
+            String.IsNullOrEmpty(sname) ||
+            String.IsNullOrEmpty(lname) ||
+            String.IsNullOrEmpty(dob) ||
+            !DateTime.TryParse(dob, out dobDate))
+        {
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Please enter data for all fields, and ensure the date is valid')", true);
+            addSuccess = false;
+        }
+        else
+        {
+            try
+            {
+                string query = "Insert Into [Author] (FirstName, SecondName, LastName, DOB) VALUES(@FirstName, @SecondName, @LastName, @DOB)";
+                SqlParameter fNameParam = new SqlParameter();
+                fNameParam.ParameterName = "@FirstName";
+                fNameParam.Value = fname;
+                SqlParameter snameParam = new SqlParameter();
+                snameParam.ParameterName = "@SecondName";
+                snameParam.Value = sname;
+                SqlParameter lnameParam = new SqlParameter();
+                lnameParam.ParameterName = "@LastName";
+                lnameParam.Value = lname;
+                SqlParameter dobParam = new SqlParameter();
+                dobParam.ParameterName = "@DOB";
+                dobParam.Value = dobDate.ToString("yyyy-MM-dd");
+
+                cmd.CommandText = query;
+                cmd.Parameters.Add(fNameParam);
+                cmd.Parameters.Add(snameParam);
+                cmd.Parameters.Add(lnameParam);
+                cmd.Parameters.Add(dobParam);
+
+                LibConnect.Open();
+                var result = cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                addSuccess = false;
+            }
+            finally
+            {
+                cmd.Dispose();
+                LibConnect.Close();
+
+                if (addSuccess)
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "Success", "alert('Author Added.')", true);
+                    tbAddAuthorFname.Text = "";
+                    tbAddAuthorSname.Text = "";
+                    tbAddAuthorLname.Text = "";
+                    tbAddAuthorDOB.Text = "";
+                }
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Something Went Wrong.')", true);
+                }
+            }
+        }
+    }
+
+    protected void btnAddIssue_Click(object sender, EventArgs e)
+    {
+        bool addSuccess = true;
+        SqlConnection LibConnect = new SqlConnection();
+        LibConnect.ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["NetClassConnectionString"].ConnectionString;
+        SqlCommand cmd = LibConnect.CreateCommand();
+
+        string bookId = tbAddIssueBookId.Text;
+        string printDate = tbAddIssuePrintDate.Text;
+        DateTime printDatetime;
+        string comments = tbAddIssueComments.Text;
+
+        if (String.IsNullOrEmpty(bookId) ||
+            String.IsNullOrEmpty(printDate) ||
+            String.IsNullOrEmpty(comments) ||
+            !DateTime.TryParse(printDate, out printDatetime))
+        {
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Please enter data for all fields and ensure the date is a proper format')", true);
+            addSuccess = false;
+        }
+        else
+        {
+            try
+            {
+                string query = "Insert Into [Issue] (BookId, PrintDate, Status, Comments) VALUES(@BookId, @PrintDate, 'Available', @Comments)";
+                SqlParameter bookIdParam = new SqlParameter();
+                bookIdParam.ParameterName = "@BookId";
+                bookIdParam.Value = bookId;
+                SqlParameter printDateParam = new SqlParameter();
+                printDateParam.ParameterName = "@PrintDate";
+                printDateParam.Value = printDatetime.ToString("yyyy-MM-dd");
+                SqlParameter commentsParam = new SqlParameter();
+                commentsParam.ParameterName = "@Comments";
+                commentsParam.Value = comments;
+
+
+                cmd.CommandText = query;
+                cmd.Parameters.Add(bookIdParam);
+                cmd.Parameters.Add(printDateParam);
+                cmd.Parameters.Add(commentsParam);
+
+                LibConnect.Open();
+                var result = cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                addSuccess = false;
+            }
+            finally
+            {
+                cmd.Dispose();
+                LibConnect.Close();
+
+                if (addSuccess)
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "Success", "alert('Issue Added.')", true);
+                    tbAddIssueBookId.Text = "";
+                    tbAddIssuePrintDate.Text = "";
+                    tbAddIssueComments.Text = "";
+                }
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Something Went Wrong.')", true);
+                }
+            }
+        }
+    }
+
+    protected void btnAddBook_Click(object sender, EventArgs e)
+    {
+        bool addSuccess = true;
+        SqlConnection LibConnect = new SqlConnection();
+        LibConnect.ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["NetClassConnectionString"].ConnectionString;
+        SqlCommand cmd = LibConnect.CreateCommand();
+
+        string authorId = tbAddBookAuthorId.Text;
+        string genreId = tbAddBookGenreId.Text;
+        string pubId = tbAddBookPubId.Text;
+        string title = tbAddBookTitle.Text;
+        string pubDate = tbAddBookPubDate.Text;
+        DateTime pubDateTime;
+        string coverType = ddCoverType.SelectedValue;
+
+        if (String.IsNullOrEmpty(authorId) ||
+            String.IsNullOrEmpty(genreId) ||
+            String.IsNullOrEmpty(pubId) ||
+            String.IsNullOrEmpty(pubDate) ||
+            String.IsNullOrEmpty(title) ||
+            !DateTime.TryParse(pubDate, out pubDateTime))
+        {
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Please enter data for all fields, and ensure the publisher date is properly formatted')", true);
+            addSuccess = false;
+        }
+        else
+        {
+            try
+            {
+                string query = "Insert Into [Book] (AuthorId, GenreId, PublisherId, Title, PublishedDate, CoverType) VALUES(@AuthorId, @GenreId, @PublisherId, @Title, @PublishedDate, @CoverType)";
+                SqlParameter authorIdParam = new SqlParameter();
+                authorIdParam.ParameterName = "@AuthorId";
+                authorIdParam.Value = authorId;
+                SqlParameter genreIdParam = new SqlParameter();
+                genreIdParam.ParameterName = "@GenreId";
+                genreIdParam.Value = genreId;
+                SqlParameter publisherIdParam = new SqlParameter();
+                publisherIdParam.ParameterName = "@PublisherId";
+                publisherIdParam.Value = pubId;
+                SqlParameter titleParam = new SqlParameter();
+                titleParam.ParameterName = "@Title";
+                titleParam.Value = title;
+                SqlParameter pubDateParam = new SqlParameter();
+                pubDateParam.ParameterName = "@PublishedDate";
+                pubDateParam.Value = pubDateTime.ToString("yyyy-MM-dd");
+                SqlParameter coverParam = new SqlParameter();
+                coverParam.ParameterName = "@CoverType";
+                coverParam.Value = coverType;
+
+                cmd.CommandText = query;
+                cmd.Parameters.Add(authorIdParam);
+                cmd.Parameters.Add(genreIdParam);
+                cmd.Parameters.Add(publisherIdParam);
+                cmd.Parameters.Add(titleParam);
+                cmd.Parameters.Add(pubDateParam);
+                cmd.Parameters.Add(coverParam);
+
+
+                LibConnect.Open();
+                var result = cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                addSuccess = false;
+            }
+            finally
+            {
+                cmd.Dispose();
+                LibConnect.Close();
+
+                if (addSuccess)
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "Success", "alert('Book Added.')", true);
+                    tbAddBookAuthorId.Text = "";
+                    tbAddBookGenreId.Text = "";
+                    tbAddBookPubId.Text = "";
+                    tbAddBookTitle.Text = "";
+                    tbAddBookPubDate.Text = "";
+                }
+                else
+                {
+                    ClientScript.RegisterClientScriptBlock(this.GetType(), "InfoMissing", "alert('Something Went Wrong.')", true);
+                }
+            }
+        }
     }
 }
---
+++ LibrarySystem/MyProfile.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Theme="SkinFile" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="MyProfile.aspx.cs" Inherits="MyProfile" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Names="Arial" Font-Size="20pt" ForeColor="#000066" Text="My Profile"></asp:Label>
+    <br />
+    <br />
+    <asp:Image ID="imgProfilePic" runat="server" Height="170px" Width="193px" />
+    <br />
+    <br />
+    <br />
+    <br />
+    <asp:Label ID="Label2" runat="server" Font-Names="Arial" ForeColor="#000066" Text="First Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="tbFName" runat="server" Width="248px"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label3" runat="server" Font-Names="Arial" ForeColor="#000066" Text="Last Name"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="tblName" runat="server" Width="248px"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label4" runat="server" Font-Names="Arial" ForeColor="#000066" Text="Address"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="tbAddress" runat="server" Width="248px"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label5" runat="server" Font-Names="Arial" ForeColor="#000066" Text="Email"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="tbEmail" runat="server" Width="248px"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label6" runat="server" Font-Names="Arial" ForeColor="#000066" Text="Profile Picture"></asp:Label>
+&nbsp;&nbsp;
+    <asp:DropDownList ID="ddProfilePic" runat="server" Height="19px" OnSelectedIndexChanged="ddProfilePic_SelectedIndexChanged" Width="258px" AutoPostBack="True">
+        <asp:ListItem Value="0">Rhino</asp:ListItem>
+        <asp:ListItem Value="1">Zebra</asp:ListItem>
+        <asp:ListItem Value="2">Giraffe</asp:ListItem>
+        <asp:ListItem Value="3">Monkey</asp:ListItem>
+    </asp:DropDownList>
+    <br />
+    <br />
+    <asp:Button ID="btnUpdate" runat="server" BackColor="#000066" Font-Names="Arial" ForeColor="White" OnClick="btnUpdate_Click" Text="Update Profile" />
+    <br />
+</asp:Content>
+
